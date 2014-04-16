d30ab313bff5f7f34678b76aa060e387bcb707b0
Erik Johnson <ej@ucar.edu>
2014-04-16 12:35:22 -0600

    fix add-box script

--- risk-mgmt-devops/vagrant-demo/1-add-box.sh
+++ risk-mgmt-devops/vagrant-demo/1-add-box.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-CMD='vagrant box add demo-sl65 ~/vagrant.sl65/erikj-sl65.20140415.box && vagrant box list'
+CMD='vagrant box add demo-sl65 ~/vagrant.sl65/erikj-sl65.20140415.box'
+
+echo $CMD
+
+$CMD
+
+CMD='vagrant box list'
 
 echo $CMD
 
